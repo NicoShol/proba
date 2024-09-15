9b373a57e5b7a886503e3498e74ddf583abc650f
Nicolas Podvin <nicolaspodvin3@gmail.com>
2024-09-15 14:19:06 +0200

    example of -log(U) ~ Exp(1)

--- uniform_transform.py
+++ uniform_transform.py
@@ -3,10 +3,14 @@ import matplotlib.pyplot as plt
 
 # Uniform transformation
 def f(x):
-    return np.sqrt(x)
+    return - np.log(x)
 
 def g(x):
-    return 2*x
+    return np.exp(-x)
+
+def define_range_transform() -> tuple:
+    """Whan applying transform to uniform values, the image may differ from 0,1"""
+    return 0.,10.
 
 def plot_histogram(samples: np.ndarray, _config: dict) -> None:
     kwargs = _config if _config else dict()
@@ -18,9 +22,10 @@ def uniform_transform(N=10_000) -> np.ndarray:
     X = np.random.random(size=N)
     return np.array([f(s) for s in X])
 
-def plot_expected_pdf(pdf: callable, n_points: int, _config: dict) -> None:
+def plot_expected_pdf(pdf: callable, interval: tuple, n_points: int, _config: dict) -> None:
     kwargs = _config if _config else dict()
-    x = np.linspace(0, 1, n_points)
+    x_min, x_max = interval
+    x = np.linspace(x_min, x_max, n_points)
     y = np.array([pdf(s) for s in x])
     plt.plot(x,y,**kwargs)
 
@@ -31,7 +36,7 @@ def main(N):
     plt.figure()
     X = uniform_transform(N)
     plot_histogram(X, CONFIG_HIST)
-    plot_expected_pdf(g, N//10, CONFIG_PLOT)
+    plot_expected_pdf(g, define_range_transform(), N//10, CONFIG_PLOT)
     plt.show()
 
 if __name__ == "__main__":
